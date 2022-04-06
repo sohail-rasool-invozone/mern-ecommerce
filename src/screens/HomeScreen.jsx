0d44258e202a0c7cb221393327eda5a6c2c382f0
Sohail Rasool <sohailrasool@mac-junaid-SSE-167.local>
2022-04-06 19:26:00 +0500

    Fetch products from api

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../data/products'
+// import products from '../data/products'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get('/api/products')
+        setProducts(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProducts()
+  }, [])
   return (
     <>
       <h3 className='text-uppercase my-3'>Latest Products</h3>
       <Row>
-        {products.map((product,index) => (
+        {products?.map((product, index) => (
           <Col key={index} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Row, Image, Col, ListGroup, Card, Button } from 'react-bootstrap'
-import products from '../data/products'
+// import products from '../data/products'
 import Rating from '../components/Rating'
 import { useParams } from 'react-router-dom'
 
 const ProductScreen = () => {
   const params = useParams()
-  const paramId = Number(params.id)
-  const product = products.find((item) => item._id === paramId)
+  const [product, setProduct] = useState([])
+  const paramId = params.id
   const { name, image, description, price, countInStock, rating, numReviews } =
     product
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/${paramId}`)
+        setProduct(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProduct()
+  }, [paramId])
   return (
     <>
       <Link className='btn btn-dark my-3' to='/'>
@@ -28,8 +41,12 @@ const ProductScreen = () => {
             <ListGroup.Item>
               <Rating value={rating} text={`${numReviews} Reviews`} />
             </ListGroup.Item>
-            <ListGroup.Item><strong>Price:</strong> {price}</ListGroup.Item>
-            <ListGroup.Item><strong>Description:</strong> {description}</ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Price:</strong> {price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Description:</strong> {description}
+            </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
@@ -44,11 +61,15 @@ const ProductScreen = () => {
               <ListGroup.Item>
                 <Row>
                   <Col>Status:</Col>
-                  <Col>{countInStock>0? 'In stock' : 'Out of Stock'}</Col>
+                  <Col>{countInStock > 0 ? 'In stock' : 'Out of Stock'}</Col>
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className='btn btn-block w-100' disabled={countInStock === 0} type='button'>
+                <Button
+                  className='btn btn-block w-100'
+                  disabled={countInStock === 0}
+                  type='button'
+                >
                   Add to cart
                 </Button>
               </ListGroup.Item>
